25fadc50cd343bd5b53c936d6acfc53ff47279d8
James Elford <james.elford2@vodafone.com>
2010-09-23 11:50:37 +0100

    Minor change - tidied up whitespace

--- src/com/vodafone360/thirdparty/HelloAidl.java
+++ src/com/vodafone360/thirdparty/HelloAidl.java
@@ -1,5 +1,7 @@
 package com.vodafone360.thirdparty;
 
+import java.lang.reflect.Field;
+
 import android.app.Activity;
 import android.content.ComponentName;
 import android.content.Intent;
@@ -14,7 +16,6 @@ import android.util.Log;
 
 import com.vodafone360.people.service.aidl.IDatabaseSubscriber;
 import com.vodafone360.people.service.aidl.IDatabaseSubscriptionService;
-import com.vodafone360.thirdparty.R;
 
 /**
  * <p>A quick and dirty demo for talking to the
@@ -36,14 +37,12 @@ import com.vodafone360.thirdparty.R;
  * 
  * <uses-permission android:name="com.vodafone360.people.service.aidl.permission.FullAccess"></uses-permission>
  * 
- * @author james
  *
  */
 public class HelloAidl extends Activity {
     
     private static final String TAG = "HelloAndroid";
 
-    
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -58,7 +57,7 @@ public class HelloAidl extends Activity {
         // Demo querying the database
         queryTheDatabase();
     }
-    
+
     /**
      * Demonstrate the usage of the ContentProvider - access
      * the 360 People database.
@@ -66,20 +65,20 @@ public class HelloAidl extends Activity {
     private void queryTheDatabase(){
         // This is how we communicate with the 360 People ContentProvider
         final Uri PROVIDER_URI = Uri.parse(com.vodafone360.people.service.aidl.Intents.DATABASE_URI);
-        
+
         // We are interested in the Activities table from the database
         Uri activitiesUri = Uri.withAppendedPath(PROVIDER_URI, "Activities");
-        
+
         // Simply query the database, as we would with any content provider
         Cursor activitiesCursor = managedQuery(activitiesUri, null, null, null, null);
-        
+
         // If the database returns Null, we're not going to have any luck at all.
         if (activitiesCursor == null) {
             Log.e(TAG, "Shouldn't have got a null pointer back from the database;" +
             		" address this!");
             return;
         }
-        
+
         /*
          * Now we'll simply loop through the cursor and dump its contents
          * to the log. It's crude, but it demonstrates what's available.
@@ -98,8 +97,7 @@ public class HelloAidl extends Activity {
             } while (activitiesCursor.moveToNext());
         }
     }
-    
-    
+
     /**
      * Demonstrates how to connect to the database so that
      * we can receive event callbacks & send commands.
@@ -110,13 +108,13 @@ public class HelloAidl extends Activity {
     private void registerForEventCallbacks(){
         Intent serviceIntent = 
             com.vodafone360.people.service.aidl.Intents.SERVICE_INTENT;
-        
+
         /* 
          * Start the service before we try to bind to it
          * as as this is less likely to fail without telling us.
          */
         startService(serviceIntent);
-        
+
         /*
          * Ask Android to bind us to this service. mServiceConnection
          * will get called when we have successfully bound, where we
@@ -128,13 +126,13 @@ public class HelloAidl extends Activity {
             Log.e(TAG, "Failed to bind to the IPC Service!");
         }
     }
-    
+
     /* 
      * This will be our handle into the engine code. Anything
      * we want to do, we'll do it with this.
      */
     private IDatabaseSubscriptionService mPeopleService;
-    
+
     /*
      * We use this to uniquely identify ourselves to the
      * 360 People client. If you use it again, you'll
@@ -142,7 +140,7 @@ public class HelloAidl extends Activity {
      */
     private final String SUBSCRIPTION_IDENTIFIER = 
         "com.vodafone360.thirdparty.HelloAidl";
-    
+
     /*
      * This is the "client-side" stuff: it's the handle the 360 People Client
      * gets on our code. The People Client directly calls code in here to let
@@ -151,7 +149,7 @@ public class HelloAidl extends Activity {
     IDatabaseSubscriber mSubscriber = new IDatabaseSubscriber.Stub() {
 
         @Override
-        public void handleEvent(Message arg0) throws RemoteException {
+        public void handleEvent(Message msg) throws RemoteException {
             Log.i(TAG, "The 360 People Client is telling us about an event!");
         }
 
@@ -160,9 +158,17 @@ public class HelloAidl extends Activity {
             Log.i(TAG, "The 360 People Client is ready to " +
             		"receive our incoming calls");
         }
-        
+
     };
-    
+
+
+    /**
+     * When you bind to a remote service on the Android platform, you have to
+     * give it an object which extends the ServiceConnection class to act as
+     * a "manager" to your connection. You'll receive notification when the
+     * connection is established (onServiceConnected), and when the connection
+     * is shut down (onServiceDisconnected)
+     */
     private final ServiceConnection mServiceConnection = new ServiceConnection(){
         @Override
         public void onServiceConnected(ComponentName name, IBinder service) {
@@ -172,7 +178,7 @@ public class HelloAidl extends Activity {
              * which gives us a handle into the engine
              */
             mPeopleService = IDatabaseSubscriptionService.Stub.asInterface(service);
-            
+
             /*
              * All calls to the mPeopleService have to be wrapped in
              * try/catch(RemoteException) blocks.
@@ -204,8 +210,6 @@ public class HelloAidl extends Activity {
             unbindService(this);
             mPeopleService = null;
         }
-        
+
     };
-    
-    
 }
